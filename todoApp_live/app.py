4cc8787f78f9e8557956d20f2eff2e9aeca97588
chalajr <chalajr2@gmail.com>
2021-08-27 09:52:50 -0500

    updated to class progress

--- todoApp_live/app.py
+++ todoApp_live/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, json, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
+tasks = [
+
+]
 ######## DataBase Functionality   ###########
 # Create Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -22,20 +25,42 @@ if not os.path.isfile('sqlite:///todo.db'):
     db.create_all()
 
 
-if not os.path.isfile('sqlite:///todo.db'):
-    db.create_all()
-
-
-@app.route("/hello", methods=['GET'])
+@app.route("/", methods=['GET'])
 def hello_world():
     return "Hello world!"
 
 
 @app.route("/tasks", methods=['GET'])
 def getTasks():
+    all_task = db.session.query(Task).all()
+    for task in all_task:
+
+        temp_task = {
+            'task_name': task.task_name,
+            'owner': task.owner,
+            'complete': task.complete,
+        }
+
+        tasks.append(temp_task)
+
     return jsonify({'task': tasks})
 
+# POST Method to add info to the array
 
-@app.route("/addTask", methods=['POST'])
-def addTasks():
-    return 0
+
+@app.route('/addTasks', methods=['POST'])
+def add_new_tasks():
+    if not request is None:
+        new_task = Task(
+            task_name=request.json['task_name'],
+            owner=request.json['owner']
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        # tasks.append(new_task)
+        return "Sucess. Record created in the database"
+    else:
+        return "Error, Request is empty"
+    return request.json
