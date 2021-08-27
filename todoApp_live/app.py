6b752e3fe42043a38303476b8d353bb8e7f6a574
chalajr <chalajr2@gmail.com>
2021-08-26 19:48:12 -0500

    Initial commit, third day of work, current sqlalchemy

--- todoApp_live/app.py
+++ todoApp_live/app.py
@@ -1,18 +1,41 @@
 from flask import Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 
+######## DataBase Functionality   ###########
+# Create Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-tasks = [
-    {
-        'task_name': 'task 1', 'owner': 'Patricio'
-    },
-    {
-        'task_name': 'task 2', 'owner': 'Pedro'
-    }
-]
+db = SQLAlchemy(app)
 
 
-@app.route("/hello/", methods=['GET'])
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(250), nullable=False)
+    owner = db.Column(db.String(250), nullable=False)
+    complete = db.Column(db.Boolean, nullable=False, default=False)
+
+
+if not os.path.isfile('sqlite:///todo.db'):
+    db.create_all()
+
+
+if not os.path.isfile('sqlite:///todo.db'):
+    db.create_all()
+
+
+@app.route("/hello", methods=['GET'])
 def hello_world():
+    return "Hello world!"
+
+
+@app.route("/tasks", methods=['GET'])
+def getTasks():
     return jsonify({'task': tasks})
+
+
+@app.route("/addTask", methods=['POST'])
+def addTasks():
+    return 0
